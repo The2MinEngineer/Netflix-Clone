c0a0cecb4d7360d64f65422f39b4908484617ea1
pheany <114763753+pheany@users.noreply.github.com>
2022-12-10 01:22:45 +0100

    This is the first commit of this project

---
+++ src/firebase.js
@@ -0,0 +1,8 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyA3WiFfhvYKHn0b-t3J4jhCngMXSyNhIaI",
+    authDomain: "netflix-clone-fc7a4.firebaseapp.com",
+    projectId: "netflix-clone-fc7a4",
+    storageBucket: "netflix-clone-fc7a4.appspot.com",
+    messagingSenderId: "1082065456771",
+    appId: "1:1082065456771:web:0ddd04886163e1c91dd5b6"
+  };
